e26202087afd104631aa685bd68c28394a16df48
Katrina Eaton <katrina.eaton@ucsf.edu>
2021-02-13 16:15:27 -0800

    Add data binding for text boxes

--- app/src/main/java/com/laborofloathing/colormyviews/MainActivity.kt
+++ app/src/main/java/com/laborofloathing/colormyviews/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.laborofloathing.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.laborofloathing.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(binding.boxOneText,
+                    binding.boxTwoText,
+                    binding.boxThreeText,
+                    binding.boxFourText,
+                    binding.boxFiveText)
+
+        for (item in clickableViews) {
+            item.setOnClickListener {makeColored(it)}
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            binding.boxOneText.id -> view.setBackgroundColor(Color.DKGRAY)
+            binding.boxTwoText.id -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            binding.boxThreeText.id -> view.setBackgroundResource(android.R.color.holo_green_light)
+            binding.boxFourText.id -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            binding.boxFiveText.id -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
